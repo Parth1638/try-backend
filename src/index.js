8ead54899c6d000af12fb77a6b1b25bc1bea5a95
Parth1638 <parthcharan1638@gmail.com>
2026-04-14 02:03:17 +0530

    feat: add cookie-parser and cors middleware, implement async error handling, and create ApiError and ApiResponse utility classes

--- src/index.js
+++ src/index.js
@@ -9,16 +9,18 @@ dotenv.config({
 })
     
 connectDB()
-
-
-
-
-
-
-
-
-
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running at Port: ${
+            process.env.PORT
+        }`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONDO db connection failed !!!",err);
+    
+})
 
 
 /*
